2fec53e6e0a3790ec2a287fda4d9fb41015211a4
yhori <yhori991@gmail.com>
2024-06-23 08:44:39 +0000

    Update gha

--- default.nix
+++ default.nix
@@ -1,14 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    NIX_ENFORCE_PURITY = false;
     nativeBuildInputs = with pkgs; [
-      quarto
       texliveFull
-      micromamba
       pixi
     ];
     shellHook = ''
-    watch_file pixi.lock
     eval "$(pixi shell-hook)"
     '';
   }
